8821219b3148de2bf7c343e1362bb6cd60571179
Sultan <280707999+salneif@users.noreply.github.com>
2026-07-06 11:42:54 +0300

    Lava Animations & Cycle

---
+++ Assets/Sultan/Scripts/Factory/LavaTrigger.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class LavaTrigger : MonoBehaviour
+{
+    [SerializeField] private string playerTag = "Player";
+
+    private PlayerController _player;
+    private bool _triggered;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (_triggered) return;
+        if (!other.CompareTag(playerTag)) return;
+
+        _player = other.GetComponent<PlayerController>();
+        if (_player == null) return;
+
+        _triggered = true;
+        _player.SetInputEnabled(false);
+    }
+
+    void OnDisable()
+    {
+        if (_triggered && _player != null)
+            _player.SetInputEnabled(true);
+        _triggered = false;
+        _player = null;
+    }
+
+    public bool IsTriggered => _triggered;
+}
