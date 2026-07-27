a734a38ae12b22c93c3f5ac71cec068628631d70
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-07-27 06:16:38 +0300

    save

--- Assets/Sultan/Scripts/Player/PlayerController.cs
+++ Assets/Sultan/Scripts/Player/PlayerController.cs
@@ -75,11 +75,13 @@ public class PlayerController : MonoBehaviour
         characterController = GetComponent<CharacterController>();
         _pusher = GetComponent<BoxPusher>();
 
+        crouchAndJumpSystem.OnCrouch += OnCrouch;
+        crouchAndJumpSystem.OnJump += OnJump;
+
         if (Camera.main != null)
             _topDownCam = Camera.main.GetComponent<TopDownCameraFollow>();
 
-        crouchAndJumpSystem.OnCrouch += OnCrouch;
-        crouchAndJumpSystem.OnJump += OnJump;
+      
     }
 
     private void OnEnable()
