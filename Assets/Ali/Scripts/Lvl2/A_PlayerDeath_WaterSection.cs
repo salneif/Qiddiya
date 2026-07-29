3d0dee14f8ec89b05d9d615706d70eaa51555e18
Sultan <280707999+salneif@users.noreply.github.com>
2026-07-29 05:11:37 +0300

    Reapply "Merge branch 'Ali3.0' into Sultan_"
    This reverts commit a3661cb4bb5ea8bf0e3e6936b90a26cbde5b8bad.

--- Assets/Ali/Scripts/Lvl2/A_PlayerDeath_WaterSection.cs
+++ Assets/Ali/Scripts/Lvl2/A_PlayerDeath_WaterSection.cs
@@ -7,6 +7,7 @@ public class A_PlayerDeath_WaterSection : MonoBehaviour
    [SerializeField] private Animator animator;
     private PlayerController playerController;
     [SerializeField] private Material deathMatrial;
+    [SerializeField] private Material OringanlMatrerial;
     [SerializeField] private GameObject mesh;
 
     private Renderer rend;
@@ -16,9 +17,14 @@ public class A_PlayerDeath_WaterSection : MonoBehaviour
     [SerializeField] private A_WaterDeathZone waterDeathZone;
     [SerializeField] private ParticleSystem waterSplash;
     public  event Action OnPlayerFall;
+    public event Action OnPlayerDeath;
 
-    
+   
 
+    private void Awake()
+    {
+        
+    }
     private void Start()
     {
         playerController = PlayerController.instance;
@@ -60,6 +66,8 @@ public class A_PlayerDeath_WaterSection : MonoBehaviour
 
         animator.SetTrigger("Death");
 
+        
+        StartPlayerDeathSystem();
     }
 
     private void OnEnemySeeingPlayer()
@@ -74,10 +82,15 @@ public class A_PlayerDeath_WaterSection : MonoBehaviour
 
 
         animator.SetTrigger("Death");
+
+        Invoke("StartPlayerDeathSystem", 5);
         
     }
-
-
+    private void StartPlayerDeathSystem()
+    {
+        OnPlayerDeath?.Invoke();
+        rend.material = OringanlMatrerial;
+    }
     private void Update()
     {
        
