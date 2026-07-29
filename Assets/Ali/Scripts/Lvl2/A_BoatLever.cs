34bc9d669433493040a209b191bf37ea6861f13d
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-07-29 07:37:26 +0300

    save

--- Assets/Ali/Scripts/Lvl2/A_BoatLever.cs
+++ Assets/Ali/Scripts/Lvl2/A_BoatLever.cs
@@ -9,6 +9,9 @@ public class A_BoatLever : MonoBehaviour
 
     [SerializeField] private int state = -1;
 
+    // Animtor ref
+    private Animator rightSideWoodAnimation;
+    private Animator leftSideWoodAnimation;
 
     public event Action<int> OnLeverSwitch;
 
@@ -25,7 +28,8 @@ public class A_BoatLever : MonoBehaviour
 
     private void Start()
     {
-        
+        rightSideWoodAnimation = rightSideWood.GetComponent<Animator>();
+        leftSideWoodAnimation = leftSideWood.GetComponent<Animator>();
     }
     private void Update()
     {
@@ -39,18 +43,23 @@ public class A_BoatLever : MonoBehaviour
             if (state == -1)
             {
                 state = 0;
+                rightSideWoodAnimation.SetBool("RightIsUp" , true);
 
                 OnLeverSwitch?.Invoke(state);
             }
-           else if(state == 0)
+          else if(state == 0)
             {
                 state = 1;
+                rightSideWoodAnimation.SetBool("RightIsUp", false);
+                leftSideWoodAnimation.SetBool("LeftIsUp" , true );
 
                 OnLeverSwitch?.Invoke(state);
             }
             else if(state == 1)
             {
                 state = 0;
+                rightSideWoodAnimation.SetBool("RightIsUp", true);
+                leftSideWoodAnimation.SetBool("LeftIsUp", false);
 
                 OnLeverSwitch?.Invoke(state);
             }
