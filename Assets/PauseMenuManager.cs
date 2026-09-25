bab79417a520c6e6b776b05447b3de3023541f00
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-09-25 18:02:05 +0300

    save

--- Assets/PauseMenuManager.cs
+++ Assets/PauseMenuManager.cs
@@ -41,6 +41,10 @@ public class PauseMenuManager : MonoBehaviour
         if (pauseCanvas != null)
         {
             pauseCanvas.enabled = isPaused;
+            if(isPaused )
+           Time.timeScale = 0;
+           else
+           Time.timeScale = 1;
         }
 
         // Deselect the button so EventSystem doesn't lock Escape key input
